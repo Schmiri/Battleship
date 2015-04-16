7c2a61510c66d73a8ed32841da5799cc0e6e4fb7
MFranke <mueri1@gmx.net>
2015-04-16 14:18:58 +0200

    asopd

---
+++ src/Korvette.java
@@ -0,0 +1,22 @@
+
+public class Korvette implements Ships {
+
+	@Override
+	public void groesse() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void schussanzahl() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void nachladen() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
