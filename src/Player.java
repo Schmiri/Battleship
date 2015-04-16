41b44faf2b7925e425f421a2b91dce2643c6db8b
mimamichelle <Michelle@dhcp-vl203-11-77.hs-bremen.de>
2015-04-16 14:09:28 +0200

    players angepasst

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,13 @@
 
 public class Player {
+	/*
+	 * 1. Auswahl eines verfügbaren Schiffes
+	 * 2. Auswahl eines Gegners.
+	 * 3. Koordinate auf dem Spielfeld auswählen
+	 * 4. Info ob getroffen wurde
+	 * 
+	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hier ist die PLayer Klasse");
