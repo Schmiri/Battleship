b5f5e22b788c3213323cce9b6ab345814a3f1a65
lkoehler92 <Lena@dhcp-vl203-12-196.hs-bremen.de>
2015-04-23 13:52:14 +0200

    bla

--- src/Spiel.java
+++ src/Spiel.java
@@ -1,12 +1,39 @@
 
+
+
+/* Klasse beinhaltet die main-Methode
+ * in der Klasse wird die Feldgröße, Schiff- und Spieleranzahl eingestellt 
+ * 
+ * 
+ */
+	
 public class Spiel {
+	
+	
+	/*Methode, die mit Hilfe einer Matrix die Feldgröße bestimmt*/
+	public int[][] feldGroeße(){
+		
+		
+	}
+	
+	/*Methode mit der man die Anzahl der Schiffe bestimmen kann*/
+	public int schiffAnzahl(){
+		
+	}
+	
+	/*Methode mit der man die Anzahl der Schiffe bestimmen kann*/
+	public int Spieleranzahl(){
+		
+		
+	}
+	
+	
 	public static void main (String args[]){
 		
 	}
-	public treffer(){
-		
-		
-	}
+	
+	
+	
 		
 	
 }
